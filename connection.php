0b253d778f6bd8a68ab3c655ae0a99bdebd73584
Roman <romires2000@gmail.com>
2020-05-31 18:15:47 +0300

    bad sql config

--- connection.php
+++ connection.php
@@ -1,3 +1,4 @@
 <?php
 include('db_data.php');
-$data = new PDO( "mysql:host=$host;dbname=$db",$user, $db_password) or die('не удалось подключиться к базе данных');
+//$data = new PDO( "mysql:host=$host;dbname=$db",$user, $db_password) or die('не удалось подключиться к базе данных');
+$data = mysqli_connect($host, $user, $db_password, $db);
